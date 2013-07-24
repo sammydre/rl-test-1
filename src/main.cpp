4001b9bd439a52d7771b452c7417d3dcdaf7c678
sammydre <sam.jansen@gmail.com>
2013-07-24 23:17:38 +0100

    Quick hack of simulator, event, entity apis.
    Not really tied together sensibly yet - but shows the simulator
    basically moving an entity about the map.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
 #include <memory>
+#include <queue>
 
 #include "libtcod.hpp"
 
@@ -59,17 +62,18 @@ struct Map
   std::unique_ptr<Tile[]> tiles_;
 };
 
-struct Player
+struct Entity
 {
-  Player()
-    : x_(40),
-      y_(25)
+  Entity(int x, int y, int ascii)
+    : x_(x),
+      y_(y),
+      ascii_(ascii)
   {
   }
 
-  void render(TCODConsole *con)
+  virtual void render(TCODConsole *con)
   {
-    con->putChar(x_, y_, '@');
+    con->putChar(x_, y_, ascii_);
     con->setCharBackground(x_, y_, TCODColor(0, 0, 0));
     con->setCharForeground(x_, y_, TCODColor(0xff, 0xff, 0xff));
   }
@@ -89,13 +93,153 @@ struct Player
 
   int x_;
   int y_;
+  int ascii_;
 };
 
+struct Player : public Entity
+{
+  Player()
+    : Entity(40, 25, '@')
+  {
+  }
+};
+
+struct Event
+{
+  Event(int64_t time)
+    : time_(time),
+      cancelled_(false),
+      reschedule_(false)
+  {
+  }
+  
+  virtual ~Event()
+  {
+  }
+
+  bool operator<(const Event &rhs) const
+  {
+    // We reverse the comparison here to make the priority queue have
+    // the lowest element at the top, not the greatest.
+    return time_ > rhs.time_;
+  }
+
+  void cancel()
+  {
+    cancelled_ = true;
+  }
+
+  virtual void run() = 0;
+
+  int64_t time_;
+  bool cancelled_;
+  bool reschedule_;
+
+  typedef std::shared_ptr<Event> Ptr;
+};
+
+struct Simulator
+{
+  Simulator()
+    : time_(0)
+  {
+  }
+
+  void step()
+  {
+    if (queue_.empty())
+      return;
+
+    std::shared_ptr<Event> ev = queue_.top();
+    queue_.pop();
+
+    if (!ev->cancelled_) {
+      fprintf(stderr, "Sim: run event at %ld\n", ev->time_);
+      time_ = ev->time_;
+      ev->run();
+    }
+
+    if (ev->reschedule_) {
+      ev->cancelled_ = false;
+      ev->reschedule_ = false;
+      add(ev);
+    }
+  }
+
+  void add(const std::shared_ptr<Event>& ev)
+  {
+    queue_.push(ev);
+  }
+
+  int64_t time_;
+  std::priority_queue<std::shared_ptr<Event>> queue_;
+};
+
+void sim_add_event(const std::shared_ptr<Event>& ev)
+{
+  // TODO
+}
+
+int64_t sim_cur_time(void)
+{
+  return 0;
+}
+
+struct ExplosionJunk : public Entity
+{
+  ExplosionJunk(int x, int y, int vel_x, int vel_y, int lifetime)
+    : Entity(x, y, '~'),
+      vel_x_(vel_x),
+      vel_y_(vel_y),
+      lifetime_(lifetime)
+  {
+  }
+
+  int vel_x_;
+  int vel_y_;
+  int lifetime_;
+
+  typedef std::shared_ptr<ExplosionJunk> Ptr;
+};
+
+struct ExplosionJunkUpdateEvent : public Event
+{
+  enum {
+    TIME_DELTA = 10
+  };
+
+  ExplosionJunkUpdateEvent(const ExplosionJunk::Ptr& j)
+    : Event(sim_cur_time() + TIME_DELTA),
+      exp_junk_(j)
+  {
+  }
+
+  virtual void run()
+  {
+    fprintf(stderr, "move by %d,%d\n", exp_junk_->vel_x_, exp_junk_->vel_y_);
+    exp_junk_->move(exp_junk_->vel_x_, exp_junk_->vel_y_);
+    exp_junk_->lifetime_ -= TIME_DELTA;
+
+    if (exp_junk_->lifetime_ > 0) {
+      reschedule_ = true;
+      time_ += TIME_DELTA;
+    } else {
+      // destroy junk?
+    }
+  }
+
+  ExplosionJunk::Ptr exp_junk_;
+};
 
 int main() 
 {
   Map map;
   Player player;
+  Simulator simulator;
+  
+  ExplosionJunk::Ptr ej(new ExplosionJunk(10, 10, 1, 1, 5000));
+
+  simulator.add(Event::Ptr(new ExplosionJunkUpdateEvent(ej)));
 
   TCODConsole::initRoot(80, 50, "libtcod test", false);
 
@@ -105,6 +249,7 @@ int main()
     console->clear();
     map.render(console);
     player.render(console);
+    ej->render(console);
 
     TCODConsole::flush();
 
@@ -124,6 +269,7 @@ int main()
       }
     }
 
+    simulator.step();
   }
   return 0;
 }
