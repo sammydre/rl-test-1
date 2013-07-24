718bc9f385c864bd3ab9b80a2eb52c7d25bb26cb
sammydre <sam.jansen@gmail.com>
2013-07-24 21:46:53 +0100

    Player class, tidyup

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,14 @@ struct Tile
 {
   Tile()
     : ascii_('#'),
-      bg_colour_(0xaa, 0xaa, 0xaa)
+      bg_colour_(0xaa, 0xaa, 0xaa),
+      fg_colour_(0xff, 0xff, 0xff)
   {
   }
 
   int ascii_;
   TCODColor bg_colour_;
+  TCODColor fg_colour_;
 };
 
 struct Map
@@ -30,6 +32,7 @@ struct Map
       for (int x = 1; x < width_ - 1; x++) {
         tile_at(x, y).ascii_ = '.';
         tile_at(x, y).bg_colour_ = TCODColor(0, 0, 0);
+        tile_at(x, y).fg_colour_ = TCODColor(0x99, 0x99, 0x99);
       }
     }
   }
@@ -41,6 +44,7 @@ struct Map
         const Tile &t = tile_at(x, y);
         con->putChar(x, y, t.ascii_);
         con->setCharBackground(x, y, t.bg_colour_);
+        con->setCharForeground(x, y, t.fg_colour_);
       }
     }
   }
@@ -55,28 +59,71 @@ struct Map
   std::unique_ptr<Tile[]> tiles_;
 };
 
+struct Player
+{
+  Player()
+    : x_(40),
+      y_(25)
+  {
+  }
+
+  void render(TCODConsole *con)
+  {
+    con->putChar(x_, y_, '@');
+    con->setCharBackground(x_, y_, TCODColor(0, 0, 0));
+    con->setCharForeground(x_, y_, TCODColor(0xff, 0xff, 0xff));
+  }
+
+  void move(int dx, int dy)
+  {
+    if (x_ + dx < 1 ||
+        x_ + dx >= 79)
+      return;
+    if (y_ + dy < 1 ||
+        y_ + dy >= 49)
+      return;
+
+    x_ += dx;
+    y_ += dy;
+  }
+
+  int x_;
+  int y_;
+};
+
 
 int main() 
 {
   Map map;
+  Player player;
 
-  int playerx=40,playery=25;
-  TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
-  while ( !TCODConsole::isWindowClosed() ) {
-    TCOD_key_t key;
-    // better to use waitForEvent so we don't spin?
-    TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
-    switch(key.vk) {
-      case TCODK_UP : playery--; break;
-      case TCODK_DOWN : playery++; break;
-      case TCODK_LEFT : playerx--; break;
-      case TCODK_RIGHT : playerx++; break;
-      default:break;
-    }
-    TCODConsole::root->clear();
-    map.render(TCODConsole::root);
-    TCODConsole::root->putChar(playerx,playery,'@');
+  TCODConsole::initRoot(80, 50, "libtcod test", false);
+
+  while (!TCODConsole::isWindowClosed()) {
+    TCODConsole *console = TCODConsole::root;
+
+    console->clear();
+    map.render(console);
+    player.render(console);
+
     TCODConsole::flush();
+
+    TCOD_key_t key;
+    TCOD_event_t event;
+
+    event = TCODSystem::waitForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL, false);
+
+    if (event == TCOD_EVENT_KEY_PRESS) {
+      switch (key.vk) {
+        case TCODK_UP:    player.move( 0, -1); break;
+        case TCODK_DOWN:  player.move( 0,  1); break;
+        case TCODK_LEFT:  player.move(-1,  0); break;
+        case TCODK_RIGHT: player.move( 1,  0); break;
+        default:
+          break;
+      }
+    }
+
   }
   return 0;
 }
